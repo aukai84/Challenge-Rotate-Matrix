175e5be745e812ecb494d5680bed625de3900a23
Aukai Tirrell <mtirrell@hawaii.edu>
2017-03-25 12:16:25 -1000

    all tests passing

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -13,14 +13,42 @@ const Direction = require("./Direction").Direction;
 module.exports = class MatrixRotator {
   constructor( matrix ) {
     this.matrix = matrix;
+    // this.arrOne = [];
+    // this.arrTwo = [];
+    // this.arrThree = [];
+    // this.bigArr2 = [];
   }
 
   //      |-- Must be Direction.CW
   //      v        or Direction.CCW
-  rotate (direction) {
+  rotate(direction){
     // do work here
 
-    // must be a valid Direction, see Direction.js
+    if(direction === Direction.CW){
+        let newMatrix = [];
+        for(let i = 0; i < this.matrix.length; i ++){
+            let newRow = [];
+            for(let j = 0; j < this.matrix[i].length; j++){
+                newRow.unshift(this.matrix[j][i]);
+            }
+            newMatrix.push(newRow)
+        }
+        this.matrix = newMatrix;
 
+    }
+    if(direction === Direction.CCW){
+        let newMatrix = [];
+        for(let i = 0; i < this.matrix.length; i ++){
+            let newRow = [];
+            for(let j = 0; j < this.matrix[i].length; j++){
+                newRow.push(this.matrix[j][i]);
+            }
+            newMatrix.unshift(newRow)
+        }
+        this.matrix = newMatrix;
+
+    }
+    // must be a valid Direction, see Direction.js
   }
+
 };
